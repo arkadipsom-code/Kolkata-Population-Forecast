3280052c2070579fc3503e2b39147b59cc3bf9d4
arkadipsom-code <arkadipsom@gmail.com>
2026-03-14 19:34:47 +0530

    Modified to Water Infrastructure Dashboard

--- app.py
+++ app.py
@@ -8,19 +8,18 @@ from sklearn.preprocessing import PolynomialFeatures
 
 # PAGE CONFIG
 st.set_page_config(
-    page_title="Kolkata's Population Forecast",
+    page_title="Kolkata's Water Infrastructure",
     layout="wide"
 )
 
 # TITLE
-st.title("Kolkata's Decedal Population Forecasting Dashboard")
+st.title("Kolkata's Decadal Population and Water Demand Forecast Dashboard")
 
-st.markdown(
-"""
-This interactive dashboard predicts **future population of Kolkata**
-using both **Civil Engineering forecasting methods** and **Machine Learning models**.
-"""
+st.info(
+"This dashboard estimates future water demand and sewer infrastructure load for Kolkata using population forecasting and standard engineering design assumptions."
 )
+
+st.divider()
 
 # LOAD DATA
 @st.cache_data
@@ -90,13 +89,13 @@ poly_pred = poly_model.predict(
     poly.transform([[target_year]])
 )[0]
 
+st.header("Population Forecast Models")
 # TABS
 tab1, tab2, tab3 = st.tabs(["Forecast", "Visualization", "Dataset"])
 
 # FORECAST TAB
 
 with tab1:
-
     st.subheader("Population Prediction")
 
     col1, col2, col3, col4 = st.columns(4)
@@ -194,3 +193,295 @@ with tab3:
     Used for forecasting future population trends.
     """
     )
+
+st.divider()
+
+st.header("Kolkata's Water Infrastructure Analysis")
+
+st.divider()
+
+# Water Demand Model
+
+
+st.subheader("Water Demand and Wastewater Generation Estiamation")
+
+st.caption("***Note: Population estimated by LinearRegression ML model is considered in the calculations of Water Demand & Wastewater Generation!***")
+
+population_wd = linear_pred
+
+# st.subheader("Water Demand Estimation for Kolkata")
+per_capita = 135 # litres per person per day (lpcd)
+
+water_demand = population_wd * per_capita
+
+
+# Wastewater Generation
+
+# st.subheader("Estimated Wastewater Generation")
+
+ww = water_demand * 0.8
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric(
+    "Estimated Water Demand (MLD)",
+    round(water_demand / 1e6, 2)
+)
+with col2:
+    st.metric(
+    "Estimated Wastewater Generation (MLD)",
+    round(ww / 1e6, 2)
+)
+    
+st.divider()
+
+st.subheader("Wastewater Quality Analysis")
+
+bod_concentration = 250  # mg/L
+cod_concentration = 500  # mg/L
+
+
+wastewater_L_day = ww 
+
+bod_load = wastewater_L_day * bod_concentration / 1e6
+cod_load = wastewater_L_day * cod_concentration / 1e6
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric("BOD Load (kg/day)", round(bod_load,2))
+
+with col2:
+    st.metric("COD Load (kg/day)", round(cod_load,2))
+
+# Pollution Chart
+
+import plotly.express as px
+
+pollution_data = {
+    "Parameter": ["BOD Load", "COD Load"],
+    "Load (kg/day)": [bod_load, cod_load]
+}
+
+df_pollution = pd.DataFrame(pollution_data)
+fig = px.bar(
+    df_pollution,
+    x="Parameter",
+    y="Load (kg/day)",
+    color="Parameter",
+    text_auto=True,
+    title="Wastewater Pollution Load"
+)
+
+fig.update_layout(template="plotly_dark")
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+
+# Sewer Flow Model
+
+st.subheader("Sewer Flow Model")
+
+avg_flow = ww
+min_flow = ww * 0.5
+peak_flow = ww * 2.5
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric("Average Flow (MLD)", round(avg_flow,2))
+
+with col2:
+    st.metric("Minimum Flow (MLD)", round(min_flow,2))
+
+with col3:
+    st.metric("Peak Flow (MLD)", round(peak_flow,2))
+
+# water flow chart
+
+flow_data = {
+    "Stage": [
+        "Water Demand",
+        "Wastewater Generated",
+        "Average Sewer Flow",
+        "Peak Sewer Flow"
+    ],
+    "Flow (MLD)": [
+        water_demand,
+        ww,
+        avg_flow,
+        peak_flow
+    ]
+}
+
+flow_df = pd.DataFrame(flow_data)
+
+fig = px.bar(
+    flow_df,
+    x="Stage",
+    y="Flow (MLD)",
+    title="Urban Water Flow Distribution"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+
+# Treatment Plant Load
+
+st.subheader("Treatment Plant Network Load")
+
+st.write("""**Important Note:** \nKolkata’s wastewater management is a combination of natural treatment via the East Kolkata Wetlands (EKW) and a growing network of mechanized Sewage Treatment Plants (STPs). 
+         \n***Here all the load calculations are done by considering STPs only!***""")
+
+plants_existing = {
+    "Garden Reach": 57,
+    "Keorapukur": 45,
+    "Bangur": 45,
+    "Dhapa": 80,
+    "Jorabagan": 45,
+    "Watgunge": 35,
+    "New Town (AA-IIC)": 31,
+    "New Town (AA-IIB)": 14,
+    "South Suburban East": 30,
+    "Bagjola": 18
+}
+
+plants_future = {
+    "Garden Reach Extension": 113.6,
+    "Palta Extension": 90.9,
+    "Hossainpur STP": 41.0,
+    "Joka (Bank Plot)": 40.0,
+    "Joka (WBSETCL Campus)": 45.0,
+    "Rania STP": 23.0
+}
+
+existing_capacity = sum(plants_existing.values())
+
+all_plants = {**plants_existing, **plants_future}
+future_capacity = sum(all_plants.values())
+
+# Load distribution (existing plants)
+
+ww_mld = ww / 1e6
+plant_load_existing = {
+    plant: ww_mld * (capacity / existing_capacity)
+    for plant, capacity in plants_existing.items()
+}
+
+# Load distribution (future plants added)
+plant_load_future = {
+    plant: ww_mld * (capacity / future_capacity)
+    for plant, capacity in all_plants.items()
+}
+
+st.caption("Wastewater Distribution (Existing Plants)")
+
+df_existing = pd.DataFrame(
+    plant_load_existing.items(),
+    columns=["Plant", "Load (MLD)"]
+)
+
+st.dataframe(df_existing)
+
+fig = px.bar(
+    df_existing,
+    x="Plant",
+    y="Load (MLD)",
+    title="Wastewater Load Distribution - Current Infrastructure"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.caption("Wastewater Distribution (After New Plants)")
+
+df_future = pd.DataFrame(
+    plant_load_future.items(),
+    columns=["Plant", "Load (MLD)"]
+)
+
+st.dataframe(df_future)
+
+fig = px.bar(
+    df_future,
+    x="Plant",
+    y="Load (MLD)",
+    title="Wastewater Load Distribution - Future Infrastructure"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+
+
+# Infra capacity comaprison
+st.subheader("Infrastructure Capacity Comparison")
+
+load_existing = (ww_mld / existing_capacity) * 100
+load_future = (ww_mld / future_capacity) * 100
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric("Current System Load (%)", round(load_existing,2))
+
+with col2:
+    st.metric("Future System Load (%)", round(load_future,2))
+
+
+# Infrastr warning
+st.subheader(" Wastewater Infrastructure Status")
+if load_existing > 100:
+    st.error("⚠ Current infrastructure overloaded")
+
+elif load_existing > 80:
+    st.warning("⚠ Current infrastructure nearing capacity")
+
+else:
+    st.success("✓ Current infrastructure sufficient")
+
+if load_future > 100:
+    st.error("⚠ Even with new plants, capacity will be insufficient")
+
+elif load_future > 80:
+    st.warning("⚠ Future infrastructure may face pressure")
+
+else:
+    st.success("✓ New treatment plants will reduce system load")
+
+st.divider()
+
+with st.expander("Engineering Assumptions"):
+    st.write("Per capita rate of sewage contributed per day = 135 L per capita per day")
+    st.write("Wastewater generation = 80% of water demand")
+    st.write("BOD concentration = 250 mg/L")
+    st.write("COD concentration = 500 mg/L")
+
+st.divider()
+
+st.subheader("Urban Water System Model")
+
+st.info(
+"""
+Population Forecast → Water Demand → Wastewater Generation → 
+Sewer Flow → Treatment Plant Load → Infrastructure Capacity
+"""
+)
+
+st.divider()
+
+st.markdown(
+"""
+<div style='text-align: center; font-size: 14px;'>
+
+© 2026 All Rights Reserved  
+
+Developed with ❤️ by **Arkadip Som**
+
+</div>
+""",
+unsafe_allow_html=True
+)
